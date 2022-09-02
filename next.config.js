ec2177c42b50b7168b020d93534761a200928e50
Meezaan-ud-Din Abdu Dhil-Jalali Wal-Ikram <m@meezaan.net>
2022-09-02 07:54:23 +0400

    Another update

--- next.config.js
+++ next.config.js
@@ -15,6 +15,14 @@ const nextConfig = {
       unoptimized: true
     },
   },
+  distDir: "_next",
+  generateBuildId: async () => {
+    if (process.env.BUILD_ID) {
+      return process.env.BUILD_ID;
+    } else {
+      return `${new Date().getTime()}`;
+    }
+  },
 }
 
 //module.exports = nextConfig
