9cb0222399f6772ddea601abe97fdf0a0c279795
Meezaan-ud-Din Abdu Dhil-Jalali Wal-Ikram <m@meezaan.net>
2022-09-02 08:01:41 +0400

    Fix committed files

--- next.config.js
+++ next.config.js
@@ -15,14 +15,7 @@ const nextConfig = {
       unoptimized: true
     },
   },
-  distDir: "_next",
-  generateBuildId: async () => {
-    if (process.env.BUILD_ID) {
-      return process.env.BUILD_ID;
-    } else {
-      return `${new Date().getTime()}`;
-    }
-  },
+//  distDir: "docs/_next",
 }
 
 //module.exports = nextConfig
